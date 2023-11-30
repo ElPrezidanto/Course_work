1edb63ab4cfba85b0330d45a679c7aa7726fc930
Enriki <enriki2004@mail.ru>
2023-11-30 18:27:23 +0300

    new

--- js/cart.js
+++ js/cart.js
@@ -47,51 +47,7 @@ function addToCartFromCatalogue(button) {
         notification.style.display = 'block';
         setTimeout(function () {
             notification.style.display = 'none';
-        }, 3000); // Уведомление исчезает спустя 3 секунды
-    }
-}
-function addToCartFromHome(button) {
-    let jsonItems = localStorage.getItem("cart");
-    console.log(`addToCart() cart: ${jsonItems}`);
-    let items = (jsonItems) ? JSON.parse(jsonItems) : [];
-
-    let compContainer = button.parentNode.parentNode.parentNode;
-    console.log(compContainer)
-    let compName = compContainer.querySelector('.comp_name').innerHTML;
-    let compPrice = priceToNumber(compContainer.querySelector('.price').innerHTML);
-    let compImage = compContainer.querySelector('img').getAttribute("src");
-
-    let item = {
-        name: compName,
-        price: compPrice,
-        image: compImage,
-        amount: 1
-    }
-
-    let itemAlreadyInCart = false;
-
-    for (let i of items) {
-        if (i.name === item.name) {
-            i.amount += 1;
-            itemAlreadyInCart = true;
-            break;
-        }
-    }
-
-    if (!itemAlreadyInCart) {
-        items.push(item);
-    }
-
-    localStorage.setItem("cart", JSON.stringify(items));
-    console.log(`Added to cart: ${compName} ${compPrice}`);
-    console.log(items);
-
-    var notification = document.getElementById('notification');
-    if (notification.style.display === 'none' || notification.style.display === '') {
-        notification.style.display = 'block';
-        setTimeout(function () {
-            notification.style.display = 'none';
-        }, 3000); // Уведомление исчезает спустя 3 секунды
+        }, 3000);
     }
 }
 
@@ -220,12 +176,14 @@ function updateCartItemAmount(button, change) {
     let itemContainer = button.parentNode.parentNode;
     let compName = itemContainer.querySelector('.comp-name').innerHTML;
 
-    for (let i of items) {
-        if (i.name === compName) {
-            i.amount += change;
-            if (i.amount < 1) {
-                i.amount = 1;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].name === compName) {
+            items[i].amount += change;
+
+            if (items[i].amount <= 0) {
+                items.splice(i, 1);
             }
+
             break;
         }
     }
